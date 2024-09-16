f1cce2fe044a1b4dc0e0e39c71481398879cd877
Matthias Zaunseder <matthias.zaunseder@hotmail.de>
2024-09-16 08:23:02 +0200

    Remove `dist` folder

--- dist/vite.d.cts
+++
@@ -1,8 +0,0 @@
-import unplugin from './index.cjs';
-import 'unplugin';
-import '@rollup/pluginutils';
-import 'openapi-types';
-
-declare const _default: typeof unplugin.vite;
-
-export = _default;
--- dist/vite.d.ts
+++
@@ -1,8 +0,0 @@
-import unplugin from './index.js';
-import 'unplugin';
-import '@rollup/pluginutils';
-import 'openapi-types';
-
-declare const _default: typeof unplugin.vite;
-
-export { _default as default };
